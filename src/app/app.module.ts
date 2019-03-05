de417a6735b210367f25fc1ac0e0f69962b43995
Slimanes <slimane_kouba@yahoo.fr>
2019-03-05 10:34:22 +0100

    premier tweet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
